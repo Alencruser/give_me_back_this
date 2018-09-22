58c78efe2fc76f91e04e8f388347a4d41947464c
Alencruser <alencruserxx@hotmail.fr>
2018-09-22 21:57:19 +0200

    semantic ui + page accueil modal

--- server.js
+++ server.js
@@ -10,25 +10,40 @@ var connection = mysql.createConnection({
   password : 'Dz7x~JX~qgqj',
   database : 'plugdj'
 });
+//variable des rooms
+var rooms=[];
 //Utilisation body parser
 app.use(bodyparser.urlencoded({ extended: false }));
 //declaration dossier public
 app.use(express.static('public'));
 //utilisation de ejs
 app.set('view engine', 'ejs');
-
+//Page accueil
 app.get('/',(req,res)=>{
-	var getRooms = "SELECT * FROM rooms";
-	connection.query(getRooms,function(error,results,fields){
+	let getRooms = "SELECT * FROM rooms";
+	connection.query(getRooms,(error,results,fields)=>{
 		if(error){
 			console.log(error);
 		}else if(results.length>0) {
+			results.forEach((bl)=>{
+				rooms.push(bl.created_at);
+			})
+			console.log(rooms[0]);
 			res.render('index',{rooms:results})
 		}
 	});
 });
-
+//Creation d'une room
 app.post('/',(req,res)=>{
-
+	let name=req.body.name;
+	let description=req.body.description;
+	let createRoom = `INSERT INTO rooms (name,description,created_at) VALUES ('${name}','${description}',NOW())`;
+	connection.query(createRoom,(error,results,field)=>{
+		if(error){
+			console.log(error);
+		}else {
+			res.redirect('/');
+		}
+	})
 })
 app.listen(8080);
